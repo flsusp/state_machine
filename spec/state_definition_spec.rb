e71e33d6806db8bd6db322725e9601635c7a6c48
Fabio Lima Santos <flsusp@gmail.com>
2016-06-25 12:59:13 -0300

    Adding the ability for multiple instances

---
+++ spec/state_definition_spec.rb
@@ -0,0 +1,110 @@
+require 'rspec'
+
+describe StateMachine do
+  context 'without any state' do
+    let(:without_states_class) do
+      Class.new do
+        include StateMachine
+      end
+    end
+
+    it 'expects class to be invalid' do
+      expect(without_states_class.valid_state_machine?).to be_falsy
+    end
+
+    it 'expects instance creation to fail' do
+      expect { without_states_class.new }.to raise_error StateMachine::InvalidState
+    end
+  end
+
+  context 'without an initial valid state' do
+    let(:not_initial_state_class) do
+      Class.new do
+        include StateMachine
+
+        state :opened
+        state :closed
+      end
+    end
+
+    it 'expects class to be invalid' do
+      expect(not_initial_state_class.valid_state_machine?).to be_falsy
+    end
+
+    it 'expects instance creation to fail' do
+      expect { not_initial_state_class.new }.to raise_error StateMachine::InvalidState
+    end
+  end
+
+  context 'with an initial valid state' do
+    let(:valid_initial_state_class) do
+      Class.new do
+        include StateMachine
+
+        state :opened, initial: true
+        state :closed
+      end
+    end
+
+    it 'expects class to be valid' do
+      expect(valid_initial_state_class.valid_state_machine?).to be_truthy
+    end
+
+    it 'expects instance creation to not fail' do
+      expect { valid_initial_state_class.new }.not_to raise_error
+    end
+  end
+
+  context 'with two initial states' do
+    it 'expects class creation to fail' do
+      expect do
+        Class.new do
+          include StateMachine
+
+          state :opened, initial: true
+          state :undefined, initial: true
+        end
+      end.to raise_error StateMachine::MultipleInitialStates
+    end
+  end
+
+  context 'with two states with the same name' do
+    it 'expects class creation to fail' do
+      expect do
+        Class.new do
+          include StateMachine
+
+          state :opened, initial: true
+          state :opened
+        end
+      end.to raise_error StateMachine::MultipleStatesWithSameName
+    end
+  end
+
+  context 'two state machines with different states' do
+    let(:first) do
+      Class.new do
+        include StateMachine
+
+        state :opened
+        state :closed
+      end
+    end
+    let(:second) do
+      Class.new do
+        include StateMachine
+
+        state :defined
+        state :undefined
+      end
+    end
+
+    it 'expects first class to have states :opened and :closed' do
+      expect(first.state_names).to eql [ :opened, :closed ]
+    end
+
+    it 'expects second class to have states :defined and :undefined' do
+      expect(second.state_names).to eql [ :defined, :undefined ]
+    end
+  end
+end
--- src/state_machine.rb
+++ src/state_machine.rb
@@ -1,27 +1,48 @@
 module StateMachine
-
   class InvalidState < StandardError; end
+  class MultipleInitialStates < StandardError; end
+  class MultipleStatesWithSameName < StandardError; end
 
-  def StateMachine.state(name)
+  module Template
+    def initialize
+      raise InvalidState if !self.class.valid_state_machine?
+      super
+    end
+
+    def valid_state_machine?
+      !states.empty? && !@initial_state.nil?
+    end
+
+    def state(name, opts = { initial: false })
+      raise MultipleStatesWithSameName if state_names.include? name
+      state = StateMachine::State.new(name)
+      states << state
+      if opts[:initial]
+        raise MultipleInitialStates unless @initial_state.nil?
+        @initial_state = state
+      end
+    end
+
+    def states
+      @states = [] if @states.nil?
+      @states
+    end
+
+    def state_names
+      states.map { |state| state.name }
+    end
   end
 
   def self.included(o)
-    o.extend(ClassMethods)
-    o.send :prepend, Initializer
+    o.extend(Template)
+    o.send :prepend, Template
   end
 
-  module Initializer
-    def initialize
-      raise InvalidState
-      super
-    end
-  end
+  class State
+    attr_reader :name
 
-  module ClassMethods
-    def valid_state_machine?
-    end
-
-    def state(name)
+    def initialize(name)
+      @name = name
     end
   end
 end
